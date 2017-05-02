28fb93fc4de5d4457dab61195e20762098381bbc
Jared Eiseman and Justin Gomez <justin@>
2017-05-02 16:09:09 -0700

    make prime sifter

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+//algorithm borrowed from stack overflow, pushing to study later
+
+var eratosthenes = function(n) {
+    // Eratosthenes algorithm to find all primes under n
+    var array = [], upperLimit = Math.sqrt(n), output = [];
+
+    // Make an array from 2 to (n - 1)
+    for (var i = 0; i < n; i++) {
+        array.push(true);
+    }
+
+    // Remove multiples of primes starting from 2, 3, 5,...
+    for (var i = 2; i <= upperLimit; i++) {
+        if (array[i]) {
+            for (var j = i * i; j < n; j += i) {
+                array[j] = false;
+            }
+        }
+    }
+
+    // All array[i] set to true are primes
+    for (var i = 2; i < n; i++) {
+        if(array[i]) {
+            output.push(i);
+        }
+    }
+
+    return output;
+};
+
+$(document).ready(function() {
+  $('form').submit(function(e) {
+    e.preventDefault();
+    // get a number from the user
+    var input = parseInt($('#input').val());
+
+    $('#output').text(eratosthenes(input).join(" "));
+
+  });
+});
